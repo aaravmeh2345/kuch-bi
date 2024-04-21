0589b0571ea0a8312fbc600c8af5d639fa4872e5
aaravmeh2345 <156667397+aaravmeh2345@users.noreply.github.com>
2024-04-21 12:13:25 +0530

    Update MENTION.js

--- modules/commands/MENTION.js
+++ modules/commands/MENTION.js
@@ -9,8 +9,8 @@ module.exports.config = {
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "100053330479434") {
-    var aid = ["100040426712109","100078234245540", "100065518342477","100013065791183" , "100064584243077"];
+  if (event.senderID !== "61553048218594") {
+    var aid = ["61553048218594","61553048218594", "61553048218594","61553048218594" , "61553048218594"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
       var msg = ["Abee Mere se baat Kar ..use mention kyu kar rhe ho😒", "Kya Hua Boss ko q Bula Rhe Ho?", "Wo Shayad Busy h😐🙄", "Tujhe sunai nhi deta mere admin ko tang na kr😒😒", "Mere boss ko tang naa karo", "Abee Bola na mere Admin ko bar bar mention mat karo ek bar me nahi smjh nahi ata", "Kya kaam hai mujhe bol", "Ek Baar me samjh ni ata , kyu mention kar rhe ho use" , "Mere Boss ko tang mt kiya kro smjhe😒"];

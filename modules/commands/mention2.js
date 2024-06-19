49af5ef5d4fca52b010eec24dca37663ec3aea35
aaravmeh2345 <156667397+aaravmeh2345@users.noreply.github.com>
2024-06-19 20:38:00 +0530

    Update mention2.js

--- modules/commands/mention2.js
+++ modules/commands/mention2.js
@@ -9,8 +9,8 @@ module.exports.config = {
   cooldowns: 1
 };
 module.exports.handleEvent = function({ api, event }) {
-  if (event.senderID !== "100094729435307") {
-    var aid = ["61551421059261" , "100094729435307" , "61551225242006"];
+  if (event.senderID !== "61559721461568") {
+    var aid = ["61559721461568" , "61559721461568" , "61559721461568"];
     for (const id of aid) {
     if ( Object.keys(event.mentions) == id) {
       var msg = ["Me Jaanu Ke Sath Busy hu , Mujhe kyu bula rahe ho" , "Abe Me bot hu mujhe mention mat kar" , "Dimag Mat khao" , "Kya hua janu mujhe bulaya🙂" , "Koi kaam ni hai kya tujhe" , "Bolo na babu" , "Meri yaad arahi tumhe itni" , "Ha meri jan😙" , "Mujhe mt bulya kro🙄" , "Tera sar f0d dena me , baar mention mt kro😒" , "Ek Bar Me smjh ni ata tujhe kya meri baat ,mujhe baar bar mention krrhe ho😒😒😒😒" , "Abee jaa na 😒" , "Mujhe Bulaya😘kya"];
